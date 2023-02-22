12391f3f9e2c7d925814554d11f6233bada7b87e
Han <shuihanho@gmail.com>
2023-02-22 14:05:41 +0800

    profile

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,5 +1,6 @@
 import { Menu, Dropdown, Button } from 'antd';
-import { LogoutOutlined, SettingOutlined } from '@ant-design/icons';
+import { SettingOutlined, LogoutOutlined, ProfileOutlined } from '@ant-design/icons';
+import { Space } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
 function Setting() {
@@ -8,18 +9,26 @@ function Setting() {
         navigate('/', { replace: true });
     };
 
+    const handleProfile = () => {
+        navigate('/profile');
+    }
+
 
     const menu = (
         <Menu>
-            <Menu.Item key="1" onClick={handleLogout}>
-                <LogoutOutlined />
+            <Menu.Item key="1" onClick={handleProfile}>
+                <ProfileOutlined /> <Space></Space>
+                Profile
+            </Menu.Item>
+            <Menu.Item key="2" onClick={handleLogout}>
+                <LogoutOutlined /> <Space></Space>
                 Logout
             </Menu.Item>
         </Menu>
     );
 
     return (
-        <Dropdown overlay={menu} trigger={['click']}>
+        <Dropdown overlay={menu} trigger={['click']} placement="topRight" >
             <Button type="text" icon={<SettingOutlined />} />
         </Dropdown>
     );
